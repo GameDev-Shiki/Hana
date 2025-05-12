f946d70d91c387880a250a38b0372113a642e4d5
Azusa <akiriin0@gmail.com>
2025-05-11 18:12:39 -0700

    Fix Typo

--- Hana/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hana/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -82,7 +82,7 @@ namespace Hana
 		HN_PROFILE_FUNCTION();
 
 		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3; 
-		HN_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire exture!");
+		HN_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
 		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 
